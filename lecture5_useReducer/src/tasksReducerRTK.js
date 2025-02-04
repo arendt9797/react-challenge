add142b54ebea6d501ec562ac6c74012b59a654d
hoki9797 <hoki9797@gmail.com>
2025-02-04 19:19:45 +0900

    reducer to RTK

---
+++ lecture5_useReducer/src/tasksReducerRTK.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialTasks = [
+    { id: 0, text: "Visit Kafka Museum", done: true },
+    { id: 1, text: "Watch a puppet show", done: false },
+    { id: 2, text: "Lennon Wall pic", done: false },
+];
+
+const tasksSlice = createSlice({
+    name: "tasks",
+    initialState: initialTasks,
+    reducers: {
+        added: (tasks, action) => {
+            tasks.push({ id: action.id, text: action.text, done: false });
+        },
+        changed: (tasks, action) => {
+            const idx = tasks.findIndex((task) => task.id === action.task.id);
+            tasks[idx] = action.task;
+        },
+        deleted: (tasks, action) => {
+            tasks = tasks.filter((t) => t.id !== action.id);
+        },
+    },
+});
+
+export const { added, changed, deleted } = tasksSlice.actions;
+export default tasksSlice.reducer;
